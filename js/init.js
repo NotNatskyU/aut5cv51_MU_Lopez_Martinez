16cb5ee535eb77bdcb4c17663700d6d44be96ea0
NatskyU <unatsky20@gmail.com>
2023-06-02 00:26:14 -0600

    Subido

---
+++ js/init.js
@@ -0,0 +1,19 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuracisón en tu sesrvidor de Firebase.  */
+// @ts-ignore  s
+firebase.initializeApp({
+    apiKey: "AIzaSyBVBbaYLMx2YnZfmFmfC4jYwK_OVmYfYzI",
+  
+    authDomain: "autentica-814e8.firebaseapp.com",
+  
+    projectId: "autentica-814e8",
+  
+    storageBucket: "autentica-814e8.appspot.com",
+  
+    messagingSenderId: "1053659560239",
+  
+    appId: "1:1053659560239:web:b5ae024509875a4b2f49da",
+  
+    measurementId: "G-BXVRECZC0Z"
+      });
+  
